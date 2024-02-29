b64b494cf37a9080111e9aad0b2f7fb6e0af4c48
Zekeriya İshak <75850002+zekeriyaishak@users.noreply.github.com>
2024-02-29 17:43:52 +0300

    Features/roles attribute development (#5)
    * Secured Operation added
    
    * Secured Operations attributes added to endpoint
    
    ---------
    
    Co-authored-by: ZEKERİYA İSHAK DOĞANAY <zishakdoganay@ogr.trakya.edu.tr>

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants.Messages;
 using Business.Validation.FluentValidation;
 using CorePackagesGeneral.Aspects.Autofac.Validation;
@@ -23,29 +24,34 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
         [ValidationAspect(typeof(ColorValidator))]
+        [SecuredOperation("Admin,Moderator")]
         public IResult AddColor(Color color)
         {
             _colorDal.Add(color);
             return new SuccessResult(Messages.ColorAdded);
         }
 
+        [SecuredOperation("Admin,Moderator")]
         public IResult DeleteColor(Color color)
         {
             _colorDal.Delete(color);
             return new SuccessResult(Messages.ColorDeleted);
         }
 
+        [SecuredOperation("Admin,Moderator,NormalUser")]
         public IDataResult<List<Color>> GetAll()
         {
             return new SuccessDataResult<List<Color>>(_colorDal.GetAll());
         }
 
+        [SecuredOperation("Admin,Moderator,NormalUser")]
         public IDataResult<List<Color>> GetAllByColorsId(int colorId)
         {
             return new SuccessDataResult<List<Color>>(_colorDal.GetAll(p=>p.Id==colorId));
         }
 
         [ValidationAspect(typeof(ColorValidator))]
+        [SecuredOperation("Admin,Moderator")]
         public IResult UpdateColor(Color color)
         {
             _colorDal.Update(color);
